719b7462c84d9f3254b7f7e05eb2ac1d4ec8283a
Jakob Lindqvist <jakob.lindqvist@chalmers.se>
2023-08-31 08:42:12 +0200

    Display test img

--- exp/show_img.py
+++ exp/show_img.py
@@ -2,6 +2,6 @@ import torch as th
 from pathlib import Path
 import matplotlib.pyplot as plt
 
-x_0 = th.load(Path.cwd() / "outputs/x_0.pth")[0, 0, :, :].detach().numpy()
+x_0 = th.load(Path.cwd() / "outputs/x_0.pth")[0, 0, :, :]  # .detach().numpy()
 plt.imshow(x_0)
 plt.show()
