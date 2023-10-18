83354a8e3b29fc56b868478b126f87666ec3031b
Jakob <jakob.lindqvist@chalmers.se>
2023-10-18 10:25:14 +0200

    Alvis sampling

--- exp/unet_test.py
+++ exp/unet_test.py
@@ -1,7 +1,11 @@
 """Prototyping script for sampling a UNet-based unconditional diffusion model for MNIST"""
 
 
+import sys
+
+sys.path.append(".")
 from pathlib import Path
+from argparse import ArgumentParser
 from src.diffusion.base import DiffusionSampler
 from src.diffusion.beta_schedules import improved_beta_schedule
 from src.model.unet import load_mnist_diff
@@ -11,14 +15,22 @@ from src.utils.vis import plot_samples_grid
 
 def main():
     num_diff_steps = 1000
-
+    args = parse_args()
     device = get_device(Device.GPU)
     models_dir = Path.cwd() / "models"
     unet = load_mnist_diff(models_dir / "uncond_unet_mnist.pt", device)
     diff_sampler = DiffusionSampler(improved_beta_schedule, num_diff_steps)
 
     samples, _ = diff_sampler.sample(unet, 100, device, (1, 28, 28))
-    plot_samples_grid(samples)
+    if args.plot:
+        plot_samples_grid(samples)
+
+
+def parse_args():
+    parser = ArgumentParser(prog="Train reconstruction classifier")
+    parser.add_argument("--num_samples", default=100, type=int, help="Number of samples")
+    parser.add_argument("--plot", action="store_true", help="enables plots")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
