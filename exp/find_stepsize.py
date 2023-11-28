8dd85d8cb1575f49f4eba244bd74d7d9e1b817d7
Jakob <jakob.lindqvist@chalmers.se>
2023-11-28 12:25:42 +0100

    Fix bug in script

--- exp/find_stepsize.py
+++ exp/find_stepsize.py
@@ -1,5 +1,9 @@
 """Script for sampling with classifier-full guidance with MCMC and adaptive step size"""
 
+import sys
+
+
+sys.path.append(".")
 from argparse import ArgumentParser
 from pathlib import Path
 import torch as th
@@ -14,7 +18,11 @@ from src.samplers.mcmc import (
 from src.model.resnet import load_classifier
 from src.utils.net import Device, get_device
 from src.diffusion.base import DiffusionSampler
-from src.diffusion.beta_schedules import improved_beta_schedule, new_sparse, respaced_timesteps, linear_beta_schedule
+from src.diffusion.beta_schedules import (
+    improved_beta_schedule,
+    respaced_beta_schedule,
+    linear_beta_schedule,
+)
 from src.model.guided_diff.unet import load_guided_diff_unet
 from src.model.guided_diff.classifier import load_guided_classifier
 from src.model.unet import load_mnist_diff
@@ -50,14 +58,12 @@ def main():
 
     T = args.num_diff_steps
     respaced_T = args.respaced_num_diff_steps
-    betas = beta_schedule(num_timesteps=T)
-    if respaced_T == T:
-        time_steps = th.tensor([i for i in range(T)])
-    elif respaced_T < T:
-        time_steps = respaced_timesteps(T, respaced_T)
-        betas = new_sparse(time_steps, betas)
-    else:
-        raise ValueError("respaced_num_diff_steps should not be higher than num_diff_steps (slower)")
+
+    betas, time_steps = respaced_beta_schedule(
+        original_betas=beta_schedule(num_timesteps=T),
+        T=T,
+        respaced_T=respaced_T,
+    )
 
     diff_sampler = DiffusionSampler(betas, time_steps, posterior_variance=posterior_variance)
     diff_sampler.to(device)
