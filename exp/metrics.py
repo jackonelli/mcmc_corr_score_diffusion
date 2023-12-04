e129cb2279faa5d6e9d695ba27ab38099704cfef
Jakob <jakob.lindqvist@chalmers.se>
2023-12-04 13:00:50 +0100

    Added Top-n acc metric

--- exp/metrics.py
+++ exp/metrics.py
@@ -19,8 +19,70 @@ from src.model.guided_diff.classifier import load_guided_classifier
 from src.guidance.base import GuidanceSampler, MCMCGuidanceSampler
 from src.guidance.classifier_full import ClassifierFullGuidance
 from src.samplers.mcmc import AnnealedHMCScoreSampler
-from src.utils.metrics import accuracy, hard_label_from_logit
+from src.utils.metrics import accuracy, hard_label_from_logit, top_n_accuracy
 from exp.utils import SimulationConfig, setup_results_dir, get_step_size
+
+
+def main():
+    args = parse_args()
+    # Setup and assign a directory where simulation results are saved.
+    classes_and_samples, config = collect_samples(args.res_dir, args.sim_name)
+    device = get_device(Device.GPU)
+
+    # Hyper/meta params
+    channels, image_size = config.num_channels, config.image_size
+    models_dir = Path.cwd() / "models"
+    if args.metric == "acc" or args.metric == "all":
+        # Load classifier
+        print("Computing accuracy")
+        classifier_path = models_dir / f"{config.classifier}.pt"
+        assert classifier_path.exists(), f"Model '{classifier_path}' does not exist."
+        classifier = load_guided_classifier(model_path=classifier_path, dev=device, image_size=image_size)
+        classifier.eval()
+
+        # compute_acc(classifier, classes_and_samples, 10, device)
+        compute_top_n_acc(classifier, classes_and_samples[:10], 10, device, 10)
+
+
+def compute_top_n_acc(classifier, classes_and_samples, batch_size, device, n):
+    pred_logits = []
+    true_classes = []
+    for i, (classes_path, samples_path) in enumerate(classes_and_samples):
+        print(f"File {i+1}/{len(classes_and_samples)}")
+        samples = th.load(samples_path)
+        num_samples = samples.size(0)
+        for batch in th.chunk(samples, num_samples // batch_size):
+            batch = batch.to(device)
+            ts = th.zeros((batch.size(0),)).to(device)
+            pred_logits.append(classifier(batch, ts).detach().cpu())
+
+        classes = th.load(classes_path).detach().cpu()
+        true_classes.append(classes)
+    pred_logits = th.cat(pred_logits, dim=0)
+    true_classes = th.cat(true_classes, dim=0)
+    acc = top_n_accuracy(pred_logits, true_classes, n)
+    print(acc)
+
+
+def compute_acc(classifier, classes_and_samples, batch_size, device):
+    pred_logits = []
+    true_classes = []
+    for i, (classes_path, samples_path) in enumerate(classes_and_samples):
+        print(f"File {i+1}/{len(classes_and_samples)}")
+        samples = th.load(samples_path)
+        num_samples = samples.size(0)
+        for batch in th.chunk(samples, num_samples // batch_size):
+            batch = batch.to(device)
+            ts = th.zeros((batch.size(0),)).to(device)
+            pred_logits.append(classifier(batch, ts).detach().cpu())
+
+        classes = th.load(classes_path).detach().cpu()
+        true_classes.append(classes)
+    pred_logits = th.cat(pred_logits, dim=0)
+    true_classes = th.cat(true_classes, dim=0)
+    acc = accuracy(hard_label_from_logit(pred_logits), true_classes)
+    print(acc)
+
 
 PATTERN = re.compile(r".*_(\d+)_(\d+).th")
 
@@ -35,7 +97,7 @@ def collect_samples(res_dir: Path, sim_name: str) -> Tuple[List[Tuple[Path, Path
     for sub_dir in res_dir.glob(f"{sim_name}_*"):
         print("Checking sub dir", sub_dir)
         config = SimulationConfig.from_json(sub_dir / "config.json")
-        assert sim_name == config.name
+        # assert sim_name == config.name
         classes.extend(sorted([path.name for path in sub_dir.glob("classes_*_*.th")]))
         classes = [sub_dir / cl for cl in classes]
         samples.extend(sorted([path.name for path in sub_dir.glob("samples_*_*.th")]))
@@ -44,22 +106,6 @@ def collect_samples(res_dir: Path, sim_name: str) -> Tuple[List[Tuple[Path, Path
     print(f"Found {len(samples)} samples")
     coll = list(zip(classes, samples))
     validate(coll)
-    return coll, config
-
-
-def tmp_samples(res_dir: Path, sim_name: str) -> Tuple[List[Tuple[Path, Path]], SimulationConfig]:
-    """Collect sampled images and corresponding classes
-
-    Results are saved to timestamped dirs to prevent overwriting, we need to collect samples from all dirs with matching sim name.
-    """
-    samples = []
-    for sub_dir in res_dir.glob(f"{sim_name}_*"):
-        print("Checking sub dir", sub_dir)
-        config = SimulationConfig.from_json(sub_dir / "config.json")
-        assert sim_name == config.name
-        samples.extend(sorted([path.name for path in sub_dir.glob("samples_*_*.th")]))
-        samples = [sub_dir / sm for sm in samples]
-    coll = list(zip([None] * len(samples), samples))
     return coll, config
 
 
@@ -72,47 +118,6 @@ def validate(coll):
         sm_1, sm_2 = tmp[1], tmp[2]
         assert cl_1 == sm_1, "Mismatched slurm ID"
         assert cl_2 == sm_2, "Mismatched batch index"
-
-
-def main():
-    args = parse_args()
-    # Setup and assign a directory where simulation results are saved.
-    # classes_and_samples, config = collect_samples(args.res_dir, args.sim_name)
-    classes_and_samples, config = tmp_samples(args.res_dir, args.sim_name)
-    device = get_device(Device.GPU)
-
-    # Hyper/meta params
-    channels, image_size = config.num_channels, config.image_size
-    models_dir = Path.cwd() / "models"
-    if args.metric == "acc" or args.metric == "all":
-        # Load classifier
-        print("Computing accuracy")
-        classifier_path = models_dir / f"{config.classifier}.pt"
-        assert classifier_path.exists(), f"Model '{classifier_path}' does not exist."
-        classifier = load_guided_classifier(model_path=classifier_path, dev=device, image_size=image_size)
-        classifier.eval()
-
-        compute_acc(classifier, classes_and_samples, 10, device)
-
-
-def compute_acc(classifier, classes_and_samples, batch_size, device):
-    pred_logits = []
-    true_classes = []
-    for _, samples_path in classes_and_samples[:1]:
-        print("file")
-        samples = th.load(samples_path)
-        num_samples = samples.size(0)
-        for batch in th.chunk(samples, num_samples // batch_size):
-            batch = batch.to(device)
-            ts = th.zeros((batch.size(0),)).to(device)
-            pred_logits.append(classifier(batch, ts).detach().cpu())
-
-        classes = th.ones((num_samples,)).long()
-        true_classes.append(classes)
-    pred_logits = th.cat(pred_logits, dim=0)
-    true_classes = th.cat(true_classes, dim=0)
-    acc = accuracy(hard_label_from_logit(pred_logits), true_classes)
-    print(acc)
 
 
 def parse_args():
--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,15 +1,24 @@
 from pathlib import Path
 import csv
 import numpy as np
+import torch as th
 
 
 def hard_label_from_logit(logit):
-    return logit.argmax()
+    return logit.argmax(dim=1)
 
 
 def accuracy(pred_class, true_class):
     """Accuracy from hard labels"""
     return (pred_class == true_class).float().mean()
+
+
+def top_n_accuracy(logits, true_class, n):
+    """Top-n accuracy"""
+    true_class = true_class.clone().reshape((true_class.size(0), 1))
+    top_n = logits.sort(descending=True, dim=1).indices[:, :n]
+    exists = th.any(th.eq(top_n, true_class), dim=1)
+    return exists.float().mean()
 
 
 def parse_diff_metrics(path: Path):
