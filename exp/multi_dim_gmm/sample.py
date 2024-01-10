994cea5da249d7bff99c753ad9e4eb51b0e00199
Jakob <jakob.lindqvist@chalmers.se>
2024-01-10 10:20:50 +0100

    Sampling in multi dim GMM

--- exp/multi_dim_gmm/sample.py
+++ exp/multi_dim_gmm/sample.py
@@ -6,6 +6,7 @@ sys.path.append(".")
 from typing import Optional
 from pathlib import Path
 from argparse import ArgumentParser
+import numpy as np
 import torch as th
 from src.diffusion.base import DiffusionSampler
 from src.diffusion.beta_schedules import (
@@ -25,9 +26,8 @@ from src.utils.seeding import set_seed
 
 def main():
     num_diff_steps = 100
-    num_classes = 4
+    num_classes = 8
     guid_scale = 1.0
-    x_dim = 3
     args = parse_args()
     set_seed(args.seed)
 
@@ -37,49 +37,53 @@ def main():
     device = get_device(Device.GPU)
 
     models_dir = Path.cwd() / "models/multi_dim_gmm"
-    diff_model = load_diff_model(models_dir / f"multi_dim_gmm_{x_dim}.pt", num_diff_steps, device, x_dim=x_dim)
-    classifier = load_classifier(
-        models_dir / f"class_t_gmm_{x_dim}.pt", num_classes, device, num_diff_steps=num_diff_steps, x_dim=x_dim
-    )
 
     betas = improved_beta_schedule(num_timesteps=num_diff_steps)
     time_steps = th.tensor([i for i in range(num_diff_steps)])
     diff_proc = DiffusionSampler(betas, time_steps, posterior_variance="beta")
-    guidance = ClassifierFullGuidance(classifier, lambda_=guid_scale)
 
-    classes = th.randint(low=0, high=num_classes, size=(args.num_samples,)).long().to(device)
+    start_dim, end_dim, num_steps = 10, 1000, 20
+    x_dims = th.linspace(np.log(start_dim), np.log(end_dim), num_steps).exp().round().long()
+    for i, x_dim in enumerate(x_dims, 1):
+        print(f"x_dim: {x_dim}, {i} / {num_steps}")
+        x_dim = int(x_dim.item())
 
-    print("Simple guidance sampling")
-    guid_sampler = GuidanceSampler(diff_model, diff_proc, guidance, diff_cond=False)
-    samples, _ = guid_sampler.sample(args.num_samples, classes, device, th.Size((x_dim,)), verbose=True)
-    samples = samples.detach().cpu()
-    classes = classes.detach().cpu()
-    th.save(samples, sim_dir / f"guid_samples_gmm.th")
-    th.save(classes, sim_dir / f"guid_classes_gmm.th")
+        classes = th.randint(low=0, high=num_classes, size=(args.num_samples,)).long().to(device)
+        diff_model = load_diff_model(models_dir / f"multi_dim_gmm_{x_dim}.pt", num_diff_steps, device, x_dim=x_dim)
+        classifier = load_classifier(
+            models_dir / f"class_t_gmm_{x_dim}.pt", num_classes, device, num_diff_steps=num_diff_steps, x_dim=x_dim
+        )
+        guidance = ClassifierFullGuidance(classifier, lambda_=guid_scale)
+        guid_sampler = GuidanceSampler(diff_model, diff_proc, guidance, diff_cond=False)
 
-    print("HMC guidance sampling")
-    mcmc_steps = 10
-    step_sizes = {int(t): 0.03 for t in range(0, num_diff_steps)}
-    n_trapets = 5
-    damping_coeff = 0.5
-    leapfrog_steps = 3
-    mcmc_sampler = AnnealedHMCScoreSampler(
-        mcmc_steps, step_sizes, damping_coeff, th.ones_like(betas), leapfrog_steps, guidance.grad
-    )
-    guid_sampler = MCMCGuidanceSampler(
-        diff_model=diff_model,
-        diff_proc=diff_proc,
-        guidance=guidance,
-        mcmc_sampler=mcmc_sampler,
-        reverse=True,
-        diff_cond=False,
-    )
-    samples, _ = guid_sampler.sample(args.num_samples, classes, device, th.Size((x_dim,)), verbose=True)
-    samples = samples.detach().cpu()
-    classes = classes.detach().cpu()
-    th.save(samples, sim_dir / f"hmc_guid_samples_gmm.th")
-    th.save(classes, sim_dir / f"hmc_guid_classes_gmm.th")
-    print(f"Results written to '{sim_dir}'")
+        samples, _ = guid_sampler.sample(args.num_samples, classes, device, th.Size((x_dim,)), verbose=True)
+        samples = samples.detach().cpu()
+        th.save(samples, sim_dir / f"guid_samples_gmm_{x_dim}.th")
+
+        if args.mcmc is not None:
+            print("HMC guidance sampling")
+            mcmc_steps = 10
+            step_sizes = {int(t): 0.03 for t in range(0, num_diff_steps)}
+            n_trapets = 5
+            damping_coeff = 0.5
+            leapfrog_steps = 3
+            mcmc_sampler = AnnealedHMCScoreSampler(
+                mcmc_steps, step_sizes, damping_coeff, th.ones_like(betas), leapfrog_steps, guidance.grad
+            )
+            guid_sampler = MCMCGuidanceSampler(
+                diff_model=diff_model,
+                diff_proc=diff_proc,
+                guidance=guidance,
+                mcmc_sampler=mcmc_sampler,
+                reverse=True,
+                diff_cond=False,
+            )
+            samples, _ = guid_sampler.sample(args.num_samples, classes, device, th.Size((x_dim,)), verbose=True)
+            samples = samples.detach().cpu()
+            th.save(samples, sim_dir / f"hmc_guid_samples_gmm_{x_dim}.th")
+            print(f"Results written to '{sim_dir}'")
+        classes = classes.detach().cpu()
+        th.save(classes, sim_dir / f"guid_classes_gmm_{x_dim}.th")
 
 
 def load_diff_model(diff_model_path, T, device, x_dim):
@@ -95,8 +99,6 @@ def parse_args():
     parser = ArgumentParser(prog="Sample from diffusion model")
     parser.add_argument("--num_samples", type=int, required=True, help="Number of samples")
     parser.add_argument("--mcmc", type=str, default=None, help="MCMC method: {'la', 'hmc'}")
-    parser.add_argument("--use_rev", action="store_true", help="Use reverse step")
-    parser.add_argument("--sample_separate", action="store_true", help="Use reverse step")
     parser.add_argument("--seed", type=Optional[int], default=None, help="Manual seed")
     parser.add_argument(
         "--sim_batch", type=int, default=0, help="Simulation batch index, indexes parallell simulations."
