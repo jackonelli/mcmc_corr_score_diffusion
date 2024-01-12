e5af254e6e33890bbf133b67a67159064f5d47fb
Anders <anders.sjoberg@fcc.chalmers.se>
2024-01-12 16:56:09 +0100

    Update simulation to handle stepsizes choices

--- exp/utils.py
+++ exp/utils.py
@@ -38,8 +38,10 @@ class SimulationConfig:
     mcmc_steps: Optional[int]
     # Value for disabling MCMC steps:
     mcmc_lower_t: Optional[int]
-    # accept ratio bounds in percent or a str indicating beta sch. based step sizes.
-    mcmc_bounds: Optional[Union[str, Tuple[float, float]]]
+    # Dict that include all necessary information for step sizes of the MCMC method
+    mcmc_stepsizes: Optional[dict]
+    # Number of trapezoidal steps
+    n_trapets: Optional[int]
     # Seed
     seed: Optional[int] = None
     # Meta
@@ -50,21 +52,24 @@ class SimulationConfig:
         with open(cfg_file_path) as cfg_file:
             cfg = json.load(cfg_file)
         cfg = SimulationConfig(**cfg)
-        if cfg.mcmc_bounds is not None:
-            if not isinstance(cfg.mcmc_bounds, str):
-                cfg.mcmc_bounds = tuple(cfg.mcmc_bounds)
         cfg.results_dir = Path(cfg.results_dir)
         cfg._validate()
         return cfg
 
     def _validate(self):
         if self.mcmc_method is not None:
-            assert self.mcmc_steps is not None and self.mcmc_bounds is not None
-            if not isinstance(self.mcmc_bounds, str):
-                for b in self.mcmc_bounds:
-                    # Safety check to find if decimal value entered instead of pct.
-                    assert b >= 1 and b <= 100
-                assert self.mcmc_bounds[0] < self.mcmc_bounds[1]
+            assert self.mcmc_stepsizes is not None
+            assert isinstance(self.mcmc_stepsizes['load'], bool)
+            if self.mcmc_stepsizes['load']:
+                assert self.mcmc_stepsizes['bounds'] is not None
+            else:
+                assert isinstance(self.mcmc_stepsizes['params']['factor'], float) or \
+                       isinstance(self.mcmc_stepsizes['params']['factor'], int)
+                assert isinstance(self.mcmc_stepsizes['params']['exponent'], float) or \
+                       isinstance(self.mcmc_stepsizes['params']['exponent'], int)
+                assert self.mcmc_stepsizes['beta_schedule'] is not None
+            if self.mcmc_method == 'la':
+                assert isinstance(self.n_trapets, int)
 
     def save(self, sim_dir: Path):
         tmp_config = deepcopy(self)
@@ -73,10 +78,10 @@ class SimulationConfig:
             json.dump(asdict(tmp_config), outfile, indent=4, sort_keys=False)
 
 
-def get_step_size(step_size_dir: Path, dataset_name: str, mcmc_method: str):
+def get_step_size(step_size_dir: Path, dataset_name: str, mcmc_method: str, mcmc_accept_bounds: str):
     # print("Warning: using steps from T_resp = 500")
     # steps = 500
-    path = step_size_dir / f"{dataset_name}_{mcmc_method}.p"
+    path = step_size_dir / f"{dataset_name}_{mcmc_method}_{mcmc_accept_bounds}.p"
     assert path.exists(), f"Step size file '{path}' not found"
     with open(path, "rb") as f:
         res = pickle.load(f)
