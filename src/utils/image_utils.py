769c3939c5a73c2ded49e26e691c3db6d00628ab
root <root@ROBIN1>
2024-01-17 20:14:09 +0100

    Update FID for .th-file

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -1,6 +1,9 @@
 import cv2
 import os
 import argparse
+import torch as th
+from pathlib import Path
+import matplotlib.pyplot as plt
 
 
 _RESIZE_MIN = 270
@@ -44,6 +47,39 @@ def change_image_extension_lower(img_path):
     fns_new = [os.path.join(img_path, fn.split('.')[0] + '.' + fn.split('.')[1].lower()) for fn in fns]
     for old, new in zip(fns_old, fns_new):
         os.rename(old, new)
+
+
+def convert_to_img(tensor):
+    imgs = ((tensor + 1) * 127.5).clamp(0, 255).to(th.uint8)
+    imgs = imgs.permute(0, 2, 3, 1)
+    imgs = imgs.contiguous()
+    return imgs
+
+
+def th_images_to_png(file_path: Path, dir_path: Path = None):
+    """
+    Converts a th-file with data on the format (num_images, x_dim, y_dim, 3) to png images
+    and places them in a folder created at 'dir_path' and if None then folder creates at file_path
+    (with the name as the file)
+
+    @param file_path: Path to file.npz
+    @param dir_path: Path to directory where the folder with the images is placed
+    """
+    images = th.load(file_path)
+    images = convert_to_img(images)
+    images = images.numpy()
+    n = images.shape[0]
+
+    if dir_path is None:
+        dir_path = file_path.parent / file_path.stem
+
+    if not dir_path.is_dir():
+        os.makedirs(dir_path)
+
+    for i in range(n):
+        plt.imsave(dir_path / '{}.jpeg'.format(str(i)), images[i])
+
+    return dir_path
 
 
 if __name__ == '__main__':
